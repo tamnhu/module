f0d7571b625e3bba6bada2d3e5f69c36064b1c32
tamnhu <33858731+tamnhu@users.noreply.github.com>
2019-11-14 16:08:33 +0700

    fix_4-module

--- 4/4-content.php
+++ 4/4-content.php
@@ -8,23 +8,23 @@ $url_path = $url_host . $matches[1][0];
 $url_path = str_replace('\\', '/', $url_path);
 ?>
 <div class="type-4">
-<div class="container">
-    <div class="row">
-    <div class="col-md-6 ">
-    <div class="ramid bgr-dt1">
-    <h4>New Smart phone</h4>
-    <h5>Big sale off 50%</h5>
-    <button class="btn-www">shop now --></button>
-    </div>
-    </div>
-    <div class="col-md-6 ">
-    <div class="ramid bgr-dh2">
-    <h4>New Smart phone</h4>
-    <h5>Big sale off 50%</h5>
-    <button class="btn-www">Shop now --></button>
-    </div>
-    </div>
-    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 ">
+                <div class="ramid bgr-dt1">
+                    <h4>New Smart phone</h4>
+                    <h5 class="aduducolor">Big sale off 50%</h5>
+                    <p class="adudu-hover">Shop now <i class="fa fa-arrow-right"></i></p>
+                </div>
+            </div>
+            <div class="col-md-6 ">
+                <div class="ramid bgr-dh2">
+                    <h4>New Smart phone</h4>
+                    <h5 class="aduducolor">Big sale off 50%</h5>
+                    <p class="adudu-hover">Shop now <i class="fa fa-arrow-right"></i></p>
+                </div>
+            </div>
+        </div>
 
     </div>
 </div>
